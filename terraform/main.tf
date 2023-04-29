347011f8302ce4740f716a54200031e15d74e0e8
Fozan Talat <fozantalat@gmail.com>
2023-04-29 16:06:25 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0"
-  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws),if you would like to preserve your tf-state online
   required_providers {
     google = {
       source  = "hashicorp/google"
